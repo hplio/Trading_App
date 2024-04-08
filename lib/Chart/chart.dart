ea42cc6f5f7fbade7a932f9c8d835a8f21f81bdf
Rajput Harsh <121711339+HarshRajput5@users.noreply.github.com>
2024-04-08 17:01:09 +0530

    chart added

---
+++ lib/Chart/chart.dart
@@ -0,0 +1,56 @@
+// import 'package:flutter/foundation.dart';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class MainPage extends StatefulWidget {
+  final String file;
+  const MainPage({super.key,required this.file});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  late String stockfile;
+  late WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    stockfile = widget.file;
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            // Update loading bar.
+          },
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) {},
+          onWebResourceError: (WebResourceError error) {},
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith('https://www.youtube.com/')) {
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadFlutterAsset('Assets/html/$stockfile');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('webview_flutter Example'),
+        ),
+        body: WebViewWidget(controller: controller),
+      ),
+    );
+  }
+}
--- lib/live_page/live_page.dart
+++ lib/live_page/live_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:trading_app/Chart/chart.dart';
 import 'package:trading_app/constants/colors.dart';
 import 'package:trading_app/constants/text.dart';
 
@@ -7,16 +8,13 @@ class LivePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final bool dark= Theme.of(context).brightness==Brightness.dark;
+    final bool dark = Theme.of(context).brightness == Brightness.dark;
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
         automaticallyImplyLeading: false,
         title: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 10,
-            vertical: 20
-          ),
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
@@ -25,14 +23,14 @@ class LivePage extends StatelessWidget {
                 style: Theme.of(context)
                     .textTheme
                     .labelMedium!
-                    .apply(color:dark? TColor.grey:TColor.darkerGrey),
+                    .apply(color: dark ? TColor.grey : TColor.darkerGrey),
               ),
               Text(
                 KtextString.homeAppBarSubTitle,
                 style: Theme.of(context)
                     .textTheme
                     .headlineMedium!
-                    .apply(color:dark? TColor.white:TColor.black),
+                    .apply(color: dark ? TColor.white : TColor.black),
               ),
             ],
           ),
@@ -40,12 +38,158 @@ class LivePage extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.all(26.0),
+          padding: EdgeInsets.all(26.0),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Text('Live page!!',
-              style: Theme.of(context).textTheme.headlineLarge,
-              )
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: SizedBox(
+                  width: double.maxFinite,
+                  // height: 100,
+                  child: ElevatedButton(
+                    style: const ButtonStyle(backgroundColor: MaterialStatePropertyAll(Colors.transparent)),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const MainPage(file: 'apple.html',)),
+                        );
+                      },
+                      child: const Text("Apple")),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: SizedBox(
+                  width: double.maxFinite,
+                  // height: 100,
+                  child: ElevatedButton(
+                    style: const ButtonStyle(backgroundColor: MaterialStatePropertyAll(Colors.transparent)),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const MainPage(file: 'amazon.html',)),
+                        );
+                      },
+                      child: const Text("Amazon")),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: SizedBox(
+                  width: double.maxFinite,
+                  // height: 100,
+                  child: ElevatedButton(
+                    style: const ButtonStyle(backgroundColor: MaterialStatePropertyAll(Colors.transparent)),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const MainPage(file: 'amd.html',)),
+                        );
+                      },
+                      child: const Text("Amd")),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: SizedBox(
+                  width: double.maxFinite,
+                  // height: 100,
+                  child: ElevatedButton(
+                    style: const ButtonStyle(backgroundColor: MaterialStatePropertyAll(Colors.transparent)),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const MainPage(file: 'google.html',)),
+                        );
+                      },
+                      child: const Text("Google")),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: SizedBox(
+                  width: double.maxFinite,
+                  // height: 100,
+                  child: ElevatedButton(
+                    style: const ButtonStyle(backgroundColor: MaterialStatePropertyAll(Colors.transparent)),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const MainPage(file: 'meta.html',)),
+                        );
+                      },
+                      child: const Text("Meta")),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: SizedBox(
+                  width: double.maxFinite,
+                  // height: 100,
+                  child: ElevatedButton(
+                    style: const ButtonStyle(backgroundColor: MaterialStatePropertyAll(Colors.transparent)),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const MainPage(file: 'microsoft.html',)),
+                        );
+                      },
+                      child: const Text("Microsoft")),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: SizedBox(
+                  width: double.maxFinite,
+                  // height: 100,
+                  child: ElevatedButton(
+                                        style: const ButtonStyle(backgroundColor: MaterialStatePropertyAll(Colors.transparent)),
+
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const MainPage(file: 'netflix.html',)),
+                        );
+                      },
+                      child: const Text("Netflix")),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: SizedBox(
+                  width: double.maxFinite,
+                  // height: 100,
+                  child: ElevatedButton(
+                    style: const ButtonStyle(backgroundColor: MaterialStatePropertyAll(Colors.transparent)),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const MainPage(file: 'nvdia.html',)),
+                        );
+                      },
+                      child: const Text("Nvdia")),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: SizedBox(
+                  width: double.maxFinite,
+                  // height: 100,
+                  child: ElevatedButton(
+                    style: const ButtonStyle(backgroundColor: MaterialStatePropertyAll(Colors.transparent)),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const MainPage(file: 'tesla.html',)),
+                        );
+                      },
+                      child: const Text("Tesla",textAlign: TextAlign.justify,),),
+                ),
+              ),
+              // Text('Live page!!',
+              // style: Theme.of(context).textTheme.headlineLarge,
+              // )
             ],
           ),
         ),
