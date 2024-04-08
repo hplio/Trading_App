6588b0f7f2fbb04446b61f81100e5a1ba0d56e25
hplio <harshilpatel9724@gmail.com>
2024-04-08 19:41:00 +0530

    adding search bar

---
+++ lib/common/custom_app_bar/custom_appbar.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax_flutter/iconsax_flutter.dart';
+import 'package:trading_app/constants/colors.dart';
+
+class CAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const CAppBar(
+      {super.key,
+      this.leading = true,
+      this.icon,
+      this.onPress,
+      this.isLeadingShow = false,
+      this.title,
+      this.action});
+
+  final bool leading;
+  final IconData? icon;
+  final void Function()? onPress;
+  final bool isLeadingShow;
+  final Widget? title;
+  final List<Widget>? action;
+
+  @override
+  Widget build(BuildContext context) {
+    final bool dark = Theme.of(context).brightness == Brightness.dark;
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: AppBar(
+        automaticallyImplyLeading: false,
+        leading: leading
+            ? IconButton(
+                onPressed: () => Get.back(),
+                icon: Icon(
+                  Iconsax.arrow_left,
+                  color: dark ? TColor.white : TColor.black,
+                ),
+              )
+            : isLeadingShow
+                ? IconButton(onPressed: onPress, icon: Icon(icon))
+                : null,
+        title: title,
+        actions: action,
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(40.0);
+}
--- lib/live_page/live_page.dart
+++ lib/live_page/live_page.dart
@@ -1,12 +1,89 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-// import 'package:get/get.dart';
 import 'package:trading_app/Chart/chart.dart';
 import 'package:trading_app/constants/colors.dart';
 import 'package:trading_app/constants/text.dart';
 
 class LivePage extends StatelessWidget {
   const LivePage({super.key});
+
+ @override
+  Widget build(BuildContext context) {
+    final bool dark = Theme.of(context).brightness == Brightness.dark;
+    return Scaffold(
+      appBar:AppBar(
+        elevation: 0,
+        automaticallyImplyLeading: false,
+        title: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                KtextString.homeAppBarTitle,
+                style: Theme.of(context)
+                    .textTheme
+                    .labelMedium!
+                    .apply(color: dark ? TColor.grey : TColor.darkerGrey),
+              ),
+              Text(
+                KtextString.homeAppBarSubTitle,
+                style: Theme.of(context)
+                    .textTheme
+                    .headlineMedium!
+                    .apply(color: dark ? TColor.white : TColor.black),
+              ),
+              
+            ],
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24,vertical: 8),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                width:double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  border: Border.all(
+                    color:dark ? TColor.grey :TColor.darkerGrey
+                  )
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8,vertical: 5),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      errorBorder: InputBorder.none,
+                      enabledBorder: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                      disabledBorder: InputBorder.none,
+                      focusedErrorBorder: InputBorder.none,
+                      hintText: 'Search Here!'
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              list(context, "apple.html", "Apple"),
+              list(context, "amazon.html", "Amazon"),
+              list(context, "amd.html", "Amd"),
+              list(context, "google.html", "Google"),
+              list(context, "meta.html", "Meta"),
+              list(context, "microsoft.html", "Microsoft"),
+              list(context, "netflix.html", "Netflix"),
+              list(context, "nvdia.html", "Nvidia"),
+              list(context, "tesla.html", "Tesla"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 
   Widget list(BuildContext context, String fileName, String stockName) {
     final bool dark = Theme.of(context).brightness == Brightness.dark;
@@ -57,55 +134,4 @@ class LivePage extends StatelessWidget {
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    final bool dark = Theme.of(context).brightness == Brightness.dark;
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        automaticallyImplyLeading: false,
-        title: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                KtextString.homeAppBarTitle,
-                style: Theme.of(context)
-                    .textTheme
-                    .labelMedium!
-                    .apply(color: dark ? TColor.grey : TColor.darkerGrey),
-              ),
-              Text(
-                KtextString.homeAppBarSubTitle,
-                style: Theme.of(context)
-                    .textTheme
-                    .headlineMedium!
-                    .apply(color: dark ? TColor.white : TColor.black),
-              ),
-            ],
-          ),
-        ),
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(24.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              list(context, "apple.html", "Apple"),
-              list(context, "amazon.html", "Amazon"),
-              list(context, "amd.html", "Amd"),
-              list(context, "google.html", "Google"),
-              list(context, "meta.html", "Meta"),
-              list(context, "microsoft.html", "Microsoft"),
-              list(context, "netflix.html", "Netflix"),
-              list(context, "nvdia.html", "Nvidia"),
-              list(context, "tesla.html", "Tesla"),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
 }
