88a15b7622c0a66b8e6d7ac384651f33af2c9bee
hplio <harshilpatel9724@gmail.com>
2024-04-09 21:26:01 +0530

    commit

--- lib/sign_in_page/widget/signin_form.dart
+++ lib/sign_in_page/widget/signin_form.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax_flutter/iconsax_flutter.dart';
-import 'package:trading_app/bottom_navigationbar/bottom_navigation.dart';
+// import 'package:trading_app/bottom_navigationbar/bottom_navigation.dart';
 import 'package:trading_app/login_page/login_page.dart';
 
 class SignInForm extends StatelessWidget {
